ef1b7940b6910934cf361066355e0d40fe87aa5d
Rohit Jadhav <rohit.jadhav010404@gmail.com>
2026-08-29 16:25:59 +0530

    Time: 3 ms (25.11%) | Memory: 44.8 MB (31.64%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+
+        for(int val : map.values()){
+            if(val > 2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
