dfc85b9671ccae9194695db5ff1705581f849352
Rohit Jadhav <rohit.jadhav010404@gmail.com>
2026-09-08 16:44:53 +0530

    Time: 49 ms (65.25%) | Memory: 74.4 MB (51.50%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -2,19 +2,15 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int i = 0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-
-        // Then we still need to process keys in descending order
         ArrayList<Integer> keys = new ArrayList<>(map.keySet());
         Collections.sort(keys, Collections.reverseOrder());
 
-        for (int num : keys) {
+        for(int num : keys){
             k = k - map.get(num);
-
-            if (k <= 0) {
+            if(k<=0){
                 return num;
             }
         }
